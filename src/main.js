3b24b287ebabbf0f32c0d9fefedb02a2afe1ccf4
Atticus White <contact@atticuswhite.com>
2015-11-21 02:45:24 -0500

    electronnnn

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
+var electron = require('electron');
+var app = electron.app;
 var Events = require('./log-reader/events');
-Events.start();
+var WindowManager = require('./windows/manager');
+
+require('crash-reporter').start();
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function () {
+  Events.start();
+  WindowManager.createWindows();
+});
---
+++ src/windows/manager.js
@@ -0,0 +1,29 @@
+var BrowserWindow = require('browser-window');
+var playerWindow;
+var opponentWindow;
+
+module.exports = {
+  createWindows: function () {
+    playerWindow = new BrowserWindow({
+      width: 200,
+      height: 500
+    });
+    opponentWindow = new BrowserWindow({
+      width: 200,
+      height: 500
+    });
+
+    playerWindow.on('closed', function () {
+      playerWindow = null;
+    });
+
+    opponentWindow.on('closed', function () {
+      opponentWindow = null
+    });
+
+    playerWindow.loadUrl('file://' + __dirname + '/window.html');
+    opponentWindow.loadUrl('file://' + __dirname + '/window.html');
+  },
+  playerWindow: playerWindow,
+  opponentWindow: opponentWindow
+};
