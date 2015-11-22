79b787cadaedf8675532621bd8cc2b96a4a9f6a0
Atticus White <contact@atticuswhite.com>
2015-11-21 22:51:39 -0500

    :fire: browserify

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,7 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify'),
     mocha = require('gulp-mocha'),
-    less = require('gulp-less'),
-    browserify = require('browserify'),
-    babelify = require('babelify'),
-    reactify = require('reactify'),
-    source = require('vinyl-source-stream');
+    less = require('gulp-less');
 
 gulp.task('test', function () {
   gulp.src('test/spec/**/*.js')
@@ -22,19 +18,6 @@ gulp.task('test', function () {
   .pipe(mocha({reporter: 'spec', growl: true}));
 });
 
-gulp.task('react', function () {
-  browserify('./src/windows/src/app.jsx')
-    .transform(babelify, {presets: ['react']})
-    // .transform(reactify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(notify({
-      title: 'React',
-      message: 'Bundle complete'
-    }));
-});
-
 gulp.task('less', function () {
   gulp.src('src/windows/less/main.less')
   .pipe(less())
@@ -46,12 +29,5 @@ gulp.task('testing', ['test'], function () {
 });
 
 gulp.task('watch', ['react', 'testing'], function () {
-  gulp.watch([
-    'src/windows/**/*.jsx',
-    'src/windows/**/*.html',
-    'src/windows/**/*.js'
-  ], ['react']);
-  gulp.watch([
-    'src/windows/less/**/*.less'
-  ], ['less']);
+  gulp.watch(['src/windows/less/**/*.less'], ['less']);
 })
