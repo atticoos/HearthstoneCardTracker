40b1fa5466ff38e75eae67fc7519e38a5f3a5e99
Atticus White <contact@atticuswhite.com>
2015-11-26 10:43:25 -0500

    react router

--- src/windows/src/deck-manager/app.jsx
+++ src/windows/src/deck-manager/app.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Login from './login';
 import Decks from './decks';
 import Hearthstats from './hearthstats-service';
+import {Router, Route, Link} from 'react-router';
+import history from './history';
+
 
 class DeckManagerApp extends React.Component {
   constructor () {
@@ -10,21 +13,34 @@ class DeckManagerApp extends React.Component {
       loggedIn: Hearthstats.isLoggedIn()
     };
   }
+  componentDidMount() {
+    if (this.state.loggedIn) {
+      history.replaceState(null, '/decks');
+    }
+  }
   onLoggedIn(token) {
     this.state.loggedIn = true;
     this.setState(this.state);
   }
-  render () {
-    if (this.state.loggedIn) {
-      return (
-        <Decks />
-      );
-    } else {
-      return (
-        <Login onLogin={token => this.onLoggedIn(token)} />
-      );
-    }
+  render() {
+    return (
+      <Router history={history}>
+        <Route path="/" component={Login} />
+        <Route path="/decks" component={Decks} />
+      </Router>
+    );
   }
+  // render () {
+  //   if (this.state.loggedIn) {
+  //     return (
+  //       <Decks />
+  //     );
+  //   } else {
+  //     return (
+  //       <Login onLogin={token => this.onLoggedIn(token)} />
+  //     );
+  //   }
+  // }
 }
 
 export default DeckManagerApp;
