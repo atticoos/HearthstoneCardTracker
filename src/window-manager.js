86241853c6646946639e81d243a0a331f7c9b9b4
Atticus White <contact@atticuswhite.com>
2015-11-26 08:49:49 -0500

    application tray and deck manager window

--- src/window-manager.js
+++ src/window-manager.js
@@ -1,11 +1,17 @@
 'use strict';
 import BrowserWindow from 'browser-window';
 import electron from 'electron';
+var {
+  Menu,
+  Tray
+} = electron;
 
 const WINDOW_WIDTH = 200;
 const WINDOW_HEIGHT = 400;
+var windowManager = {};
 var playerWindow;
 var opponentWindow;
+var deckManagerWindow;
 
 function createWindow (options) {
   return new BrowserWindow(Object.assign({
@@ -18,38 +24,61 @@ function createWindow (options) {
   }, options || {}));
 }
 
-module.exports = {
-  createWindows: () => {
-    var display = electron.screen.getPrimaryDisplay();
-    playerWindow = createWindow({
-      x: display.bounds.width - WINDOW_WIDTH,
-      y: 10
-    });
-    opponentWindow = createWindow({
-      x: 0,
-      y: 10
-    });
+function createDeckmanagerWindow (options) {
+  return new BrowserWindow(Object.assign({
+    width: 500,
+    height: 700
+  }, options || {}));
+}
 
-    playerWindow.on('closed', () => {
-      playerWindow = null;
-    });
+windowManager.createWindows = () => {
+  var display = electron.screen.getPrimaryDisplay();
+  playerWindow = createWindow({
+    x: display.bounds.width - WINDOW_WIDTH,
+    y: 10
+  });
+  opponentWindow = createWindow({
+    x: 0,
+    y: 10
+  });
 
-    opponentWindow.on('closed', () => {
-      opponentWindow = null
-    });
+  playerWindow.on('closed', () => {
+    playerWindow = null;
+  });
 
-    playerWindow.loadUrl('file://' + __dirname + '/windows/player-window.html');
-    opponentWindow.loadUrl('file://' + __dirname + '/windows/opponent-window.html');
+  opponentWindow.on('closed', () => {
+    opponentWindow = null
+  });
 
-    // playerWindow.webContents.openDevTools();
-    // opponentWindow.webContents.openDevTools();
-  },
-  updatePlayerWindowCards: (cards) => {
-    playerWindow.webContents.send('/player', {cards: cards});
-  },
-  updateOpponentWindowCards: (cards) => {
-    opponentWindow.webContents.send('/opponent', {cards: cards});
-  },
-  playerWindow: playerWindow,
-  opponentWindow: opponentWindow
+  playerWindow.loadUrl('file://' + __dirname + '/windows/player-window.html');
+  opponentWindow.loadUrl('file://' + __dirname + '/windows/opponent-window.html');
+
+  // playerWindow.webContents.openDevTools();
+  // opponentWindow.webContents.openDevTools();
+
+  var applicationTray = new Tray(__dirname + '/tray.png');
+  var contextMenu = Menu.buildFromTemplate([
+    {label: 'Manage Decks', click: () => windowManager.openDeckManager()}
+  ]);
+  applicationTray.setContextMenu(contextMenu);
 };
+
+windowManager.openDeckManager = () => {
+  if (deckManagerWindow) {
+    return;
+  }
+  deckManagerWindow = createDeckmanagerWindow();
+  deckManagerWindow.loadUrl('file://' + __dirname + '/windows/deck-manager-window.html');
+};
+
+windowManager.updatePlayerWindowCards = (cards) => {
+  playerWindow.webContents.send('/player', {cards: cards});
+};
+windowManager.updateOpponentWindowCards = (cards) => {
+  opponentWindow.webContents.send('/opponent', {cards: cards});
+};
+
+windowManager.playerWindow = playerWindow;
+windowManager.opponentWindow = opponentWindow;
+
+export default windowManager;
